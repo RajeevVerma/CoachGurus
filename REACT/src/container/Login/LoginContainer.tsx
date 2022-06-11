e2356f8de646c3617d1fd207b0fbd3cb0d30b0f3
dongre17 <AnshulDongre2017@hotmail.com>
2022-06-11 14:58:13 +0530

    login with google oauth2

--- REACT/src/container/Login/LoginContainer.tsx
+++ REACT/src/container/Login/LoginContainer.tsx
@@ -1,6 +1,8 @@
 // Import components
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonRow } from '@ionic/react';
+import { IonButton, IonCol, IonGrid, IonHeader, IonIcon, IonRow } from '@ionic/react';
 import { isPlatform } from '@ionic/react';
+
+import { GoogleAuth, User } from "@codetrix-studio/capacitor-google-auth";
 
 // Import Icons
 import { logoGoogle, logoApple, logoFacebook, mailOutline } from "ionicons/icons";
@@ -19,6 +21,19 @@ const LoginContainer: React.FC<ContainerProps> = () => {
         || isPlatform('cordova')
         || isPlatform('capacitor');
 
+    GoogleAuth.initialize({
+        clientId: '1042016617653-jderfq6e5acm8mh0ql3v096mpb2kscua.apps.googleusercontent.com',
+        scopes: ['profile', 'email'],
+        grantOfflineAccess: true,
+    });
+
+    const loginWithGoogle = () => {
+        GoogleAuth.signIn().
+            then((response: User) => {
+                debugger;
+            });
+    }
+
     return (
         <IonGrid className='login-grid'>
             <IonRow className='login-grid-row'>
@@ -27,26 +42,29 @@ const LoginContainer: React.FC<ContainerProps> = () => {
                 </IonCol>
                 <IonCol className='login-right-panel'>
                     <IonHeader className='login-right-panel-h1'>
-                        Join over 25 million people who use Sworkit for custom workouts anytime anywhere.                        
+                        Join over 25 million people who use Sworkit for custom workouts anytime anywhere.
                     </IonHeader>
                     <div className='login-action'>
-                            <IonButton expand="block" className='login-apple'>
-                                <IonIcon slot="start" icon={logoApple}></IonIcon>
-                                Signin with Apple
-                            </IonButton>
-                            <IonButton expand="block" className='login-google'>
-                                <IonIcon slot="start" icon={logoGoogle}></IonIcon>
-                                Signin with Google
-                            </IonButton>
-                            <IonButton expand="block" className='login-facebook'>
-                                <IonIcon slot="start" icon={logoFacebook}></IonIcon>
-                                Signin with Facebook
-                            </IonButton>
-                            <IonButton expand="block" className='login-custom-mail'>
-                                <IonIcon slot="start" icon={mailOutline}></IonIcon>
-                                Signin with Facebook
-                            </IonButton>
-                        </div>
+                        <IonButton expand="block" className='login-apple'>
+                            <IonIcon slot="start" icon={logoApple}></IonIcon>
+                            Signin with Apple
+                        </IonButton>
+                        <IonButton
+                            expand="block"
+                            className='login-google'
+                            onClick={() => loginWithGoogle()}>
+                            <IonIcon slot="start" icon={logoGoogle}></IonIcon>
+                            Signin with Google
+                        </IonButton>
+                        <IonButton expand="block" className='login-facebook'>
+                            <IonIcon slot="start" icon={logoFacebook}></IonIcon>
+                            Signin with Facebook
+                        </IonButton>
+                        <IonButton expand="block" className='login-custom-mail'>
+                            <IonIcon slot="start" icon={mailOutline}></IonIcon>
+                            Signin with Facebook
+                        </IonButton>
+                    </div>
                 </IonCol>
             </IonRow>
         </IonGrid>
