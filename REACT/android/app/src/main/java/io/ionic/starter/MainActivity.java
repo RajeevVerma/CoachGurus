8f84ddccd05171262f7ff16fe34e490941262f3e
dongre17 <AnshulDongre2017@hotmail.com>
2022-06-21 09:54:52 +0530

    Added into android config FB n Google

--- REACT/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ REACT/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,17 @@
 package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
-public class MainActivity extends BridgeActivity {}
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>> {{
+            add(GoogleAuth.class);
+        }}
+    }
+}
