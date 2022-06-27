e259d627a057005014b0bed2951ccb45a505916f
dongre17 <AnshulDongre2017@hotmail.com>
2022-06-27 08:56:25 +0530

    fixed google login

--- REACT/src/setupProxy.js
+++ REACT/src/setupProxy.js
@@ -1,10 +1,11 @@
-module.exports = {
-  developMiddleware: app => {
-    app.use(
-      "/api",
-      proxy({
-        target: "http://localhost:3000",
-      })
-    )
-  },
-}
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api/",
+    createProxyMiddleware({
+      target: 'http://localhost:3000',
+      changeOrigin: true,
+    })
+  );
+};
