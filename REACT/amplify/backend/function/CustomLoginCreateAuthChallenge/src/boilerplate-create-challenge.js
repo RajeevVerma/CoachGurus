f9461fd002860bcf133cb40861133e1866ab023f
dongre17 <AnshulDongre2017@hotmail.com>
2022-07-05 17:14:38 +0530

    using custom otp for development

--- REACT/amplify/backend/function/CustomLoginCreateAuthChallenge/src/boilerplate-create-challenge.js
+++ REACT/amplify/backend/function/CustomLoginCreateAuthChallenge/src/boilerplate-create-challenge.js
@@ -4,41 +4,44 @@ const AWS = require('aws-sdk');
 
 exports.handler = (event, context, callback) => {
   //Create a random number for otp
-  const challengeAnswer = Math.random().toString(10).substr(2, 6);
+  let challengeAnswer = Math.random().toString(10).substr(2, 6);
   const phoneNumber = event.request.userAttributes.phone_number;
 
   //For Debugging
   console.log(event, context);
 
-  //sns sms
-  const sns = new AWS.SNS({ region: 'ap-south-1' });
-  sns.publish(
-    {
-      Message: 'your otp: ' + challengeAnswer,
-      PhoneNumber: phoneNumber,
-      MessageStructure: 'string',
-      MessageAttributes: {
-        'AWS.SNS.SMS.SenderID': {
-          DataType: 'String',
-          StringValue: 'AMPLIFY',
-        },
-        'AWS.SNS.SMS.SMSType': {
-          DataType: 'String',
-          StringValue: 'Transactional',
+  if (phoneNumber === '+918380088317') {
+    //sns sms
+    const sns = new AWS.SNS({ region: 'ap-south-1' });
+    sns.publish(
+      {
+        Message: 'your otp: ' + challengeAnswer,
+        PhoneNumber: phoneNumber,
+        MessageStructure: 'string',
+        MessageAttributes: {
+          'AWS.SNS.SMS.SenderID': {
+            DataType: 'String',
+            StringValue: 'AMPLIFY',
+          },
+          'AWS.SNS.SMS.SMSType': {
+            DataType: 'String',
+            StringValue: 'Transactional',
+          },
         },
       },
-    },
-    function (err, data) {
-      if (err) {
-        console.log(err.stack);
-        console.log(data);
-        return;
+      function (err, data) {
+        if (err) {
+          console.log(err.stack);
+          console.log(data);
+          return;
+        }
+        console.log(`SMS sent to ${phoneNumber} and otp = ${challengeAnswer}`);
+        return data;
       }
-      console.log(`SMS sent to ${phoneNumber} and otp = ${challengeAnswer}`);
-      return data;
-    }
-  );
-
+    );
+  } else {
+    challengeAnswer = 9808;
+  }
   //set return params
   event.response.privateChallengeParameters = {};
   event.response.privateChallengeParameters.answer = challengeAnswer;
