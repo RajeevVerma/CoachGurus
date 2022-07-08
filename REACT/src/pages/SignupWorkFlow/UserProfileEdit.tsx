14f1d96d66a65e4a51373aee2b42f22d62263cfd
dongre17 <AnshulDongre2017@hotmail.com>
2022-07-08 15:09:23 +0530

    selection and diselection of categories

--- REACT/src/pages/SignupWorkFlow/UserProfileEdit.tsx
+++ REACT/src/pages/SignupWorkFlow/UserProfileEdit.tsx
@@ -8,6 +8,7 @@ import {
 } from '@ionic/react';
 import { UserType } from 'enums';
 import { ICognitoUser } from 'models';
+import { useState } from 'react';
 
 import './UserProfileEdit.css';
 
@@ -16,7 +17,86 @@ interface IUserProfileEditPageProps {
   userType: UserType;
 }
 
+interface ICategories {
+  value: string;
+  key: string;
+  selected?: boolean;
+  child?: ICategories[];
+}
+
+const rootInterest: ICategories[] = [
+  {
+    key: 'S-1',
+    value: 'Sports',
+    child: [
+      {
+        key: 'S-1-1',
+        value: 'Cricket',
+      },
+      {
+        key: 'S-1-2',
+        value: 'Volleyball',
+      },
+      {
+        key: 'S-1-3',
+        value: 'Badminton',
+      },
+    ],
+  },
+  {
+    key: 'A-1',
+    value: 'Academics',
+    child: [
+      {
+        key: 'A-1-1',
+        value: 'Maths',
+        child: [
+          {
+            key: 'A-1-1-1',
+            value: 'Primary School',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    key: 'E-1',
+    value: 'Extra-Curricular',
+  },
+];
+
 function UserProfileEditPage(props: IUserProfileEditPageProps): JSX.Element {
+  let [categories, setCategories] = useState<ICategories[]>(rootInterest);
+
+  const handleCategoryClick = (category: ICategories) => {
+    const categoryIndex = categories.findIndex((x) => x.key === category.key);
+
+    if (categoryIndex > -1 && Array.isArray(category.child)) {
+      if (categories[categoryIndex].selected) {
+        categories[categoryIndex].selected = false;
+        categories = categories.filter(
+          (x) => !x.key.startsWith(`${category.key}-`)
+        );
+      } else {
+        categories[categoryIndex].selected = true;
+        categories = [...categories, ...category.child];
+      }
+      setCategories(categories);
+    }
+  };
+
+  const categorySelection = (categories: ICategories[]) => (
+    <IonItem>
+      {categories.map((category: ICategories) => (
+        <IonChip
+          key={category.key}
+          onClick={() => handleCategoryClick(category)}>
+          <IonLabel>{category.value}</IonLabel>
+        </IonChip>
+      ))}
+    </IonItem>
+  );
+
   return (
     <IonPage className='user-profile'>
       <IonContent fullscreen={false}>
@@ -39,17 +119,8 @@ function UserProfileEditPage(props: IUserProfileEditPageProps): JSX.Element {
           <IonLabel>User Type</IonLabel>
           <IonInput type='text' id='user-type' />
         </IonItem>
-        <IonItem>
-          <IonChip>
-            <IonLabel>Sports</IonLabel>
-          </IonChip>
-          <IonChip>
-            <IonLabel>Academics</IonLabel>
-          </IonChip>
-          <IonChip>
-            <IonLabel>Extra Curricular</IonLabel>
-          </IonChip>
-        </IonItem>
+
+        {categorySelection(categories)}
       </IonContent>
     </IonPage>
   );
