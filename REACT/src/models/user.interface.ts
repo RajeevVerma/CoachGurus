345b5bb3309c145282dbcd838bfdce98600c1aa6
dongre17 <AnshulDongre2017@hotmail.com>
2022-07-15 11:56:51 +0530

    updated changes for images

--- REACT/src/models/user.interface.ts
+++ REACT/src/models/user.interface.ts
@@ -1,54 +1,64 @@
 import { UserSignUpSource, UserType } from 'enums';
 
 export interface IResponse {
-    Item: IUser;
+  Item: IUser;
 }
 
-
 export interface IUser {
-    email?: string;
-    firstName?: string;
-    lastName?: string;
+  email?: string;
+  firstName?: string;
+  lastName?: string;
 
-    city?: string; // GSI
-    signUpSourceType: UserSignUpSource;
-    mobilePhone: string;
-    alternatePhone?: string;
-    userType?: UserType;
+  city?: string; // GSI
+  signUpSourceType: UserSignUpSource;
+  mobilePhone: string;
+  alternatePhone?: string;
+  userType?: UserType;
 
-    /** Auto set on backend */
-    signUpDate?: Date;
-    lastLoginDate?: Date;
-    profilePicUrl?: string;
-    bucketFolderName: string;
+  /** Auto set on backend */
+  signUpDate?: Date;
+  lastLoginDate?: Date;
+  profilePicUrl?: string;
+  bucketFolderName: string;
 
-    profileData?: {
-        shortBio?: string;
-        description?: string;
-        /**store | separated if multiple */
-        certifications?: string;
+  profileData?: {
+    shortBio?: string;
+    description?: string;
+    /**store | separated if multiple */
+    certifications?: string;
 
-        /**coach qualifications | separated*/
-        qualifications?: string;
-        /**store | separated if multiple */
-        coachingPhotos?: string;
-        finalRatings?: number;
-        yearsOfExperience?: number;
-    };
+    /**coach qualifications | separated*/
+    qualifications?: string;
+    /**store | separated if multiple */
+    coachingPhotos?: string;
+    finalRatings?: number;
+    yearsOfExperience?: number;
+  };
 
-    /** To show any badge  */
-    isCoachGuruVerified?: boolean;
+  /** To show any badge  */
+  isCoachGuruVerified?: boolean;
 
-    /* pipe separated */
-    locationPks?: string;
+  /* pipe separated */
+  locationPks?: string;
 
-    phoneOtpVerified?: boolean; // TODO: Can be indexed
-    emailOtpVerified?: boolean;
+  phoneOtpVerified?: boolean; // TODO: Can be indexed
+  emailOtpVerified?: boolean;
 
-    /**
-     * Endeavour ids | separated
-     */
-    coachingEndeavourPks: string;
-    /** Should be sorted based on priority */
-    // endeavourTypes?: EndeavourCategory[];
+  /**
+   * Endeavour ids | separated
+   */
+  coachingEndeavourPks: string;
+  /** Should be sorted based on priority */
+  // endeavourTypes?: EndeavourCategory[];
+}
+
+export interface IFile {
+  extension: string;
+  id: string;
+  preview: {
+    type: string;
+    url: string;
+  };
+  size: number;
+  type: string;
 }
